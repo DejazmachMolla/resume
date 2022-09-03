015186cc1045eda37900db6b730d757cad3e5581
Dejazmach <jilogical@gmail.com>
2022-09-03 21:22:44 +0300

    Made the intro text equal in height with the image of the intro

--- script.js
+++ script.js
@@ -33,25 +33,27 @@
     document.getElementsByClassName("small-image")[0].style.width = "100%";
     document.getElementsByClassName("small-image")[0].style.borderRadius = "20px";
     document.getElementsByClassName("imagencontact")[0].style.width = "calc(35% - 2em)";
+    document.getElementById("text-content").style.width = "auto";
+    
   }
 
   function hideDeveloperIntroText() {
     document.getElementById("text-content").style.display = "none";
-      document.getElementById("text-content").style.width = "0";
-      document.getElementsByClassName("imagencontact")[0].style.borderTopLeftRadius = "6px";
-      document.getElementsByTagName("header")[0].style.right = "0";
-      document.getElementsByTagName("header")[0].style.top = "0";
-      document.getElementsByClassName("contact-links")[0].style.display = "flex";
-      document.getElementsByClassName("contact-links")[0].style.flexDirection = "column";
-      document.getElementsByClassName("contact-links")[0].style.alignItems = "center";
-      var linkTexts = document.getElementsByClassName("external-link");
-      for (var i = 0; i < linkTexts.length; i++) {
-        linkTexts[i].style.display = "none";
-      }
+    document.getElementById("text-content").style.width = "0";
+    document.getElementsByClassName("imagencontact")[0].style.borderTopLeftRadius = "6px";
+    document.getElementsByTagName("header")[0].style.right = "0";
+    document.getElementsByTagName("header")[0].style.top = "0";
+    document.getElementsByClassName("contact-links")[0].style.display = "flex";
+    document.getElementsByClassName("contact-links")[0].style.flexDirection = "column";
+    document.getElementsByClassName("contact-links")[0].style.alignItems = "center";
+    var linkTexts = document.getElementsByClassName("external-link");
+    for (var i = 0; i < linkTexts.length; i++) {
+      linkTexts[i].style.display = "none";
+    }
 
-      document.getElementsByClassName("small-image")[0].style.width = "40px";
-      document.getElementsByClassName("small-image")[0].style.borderRadius = "100%";
-      document.getElementsByClassName("imagencontact")[0].style.width = "40px";
+    document.getElementsByClassName("small-image")[0].style.width = "40px";
+    document.getElementsByClassName("small-image")[0].style.borderRadius = "100%";
+    document.getElementsByClassName("imagencontact")[0].style.width = "40px";
   }
   var prevScrollpos = 150; 
   var minTextAllowingWidth = 600;
@@ -62,6 +64,9 @@
   function hideDeveloperIntroForMobile() {
     if(minTextAllowingWidth > getCurrentBrowserWidth()) {
       hideDeveloperIntroText();
+    } else {
+      document.getElementById("text-content").style.height = `${document.getElementById("imagencontact").offsetWidth -24}px`;
+      document.getElementById("text-content").style.width = "auto";
     }
   }
   
